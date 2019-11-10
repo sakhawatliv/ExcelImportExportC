90c0ad2c05b913246e22b317099acf3bfc4dec46
sakhawatliv <sakhawatliv@gmail.com>
2019-11-11 01:28:35 +0600

    excel readed

---
+++ ExcelExport/Exel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelExport
+{
+    public class Exel
+    {
+        string path = "";
+        _Application excel = new _Excel.Application();
+        Workbook wb;
+        Worksheet ws;
+
+        public Exel(string path, int Sheet)
+        {
+            this.path = path;
+            wb = excel.Workbooks.Open(path);
+            ws = wb.Worksheets[Sheet];
+
+        }
+        public string ReadCell(int i, int j)
+        {
+            i++;
+            j++;
+            if (ws.Cells[i, j].Value2 != null)
+            {
+                return ws.Cells[i, j].Value2;
+            }
+            else
+            {
+                return "";
+            }
+
+        }
+        
+    }
+}
---
+++ ExcelExport/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace ExcelExport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exel excel = new Exel(@"C:\Test.xlsx", 1);
+            Console.WriteLine(excel.ReadCell(0, 0));
+            Console.ReadKey();
+        }
+    }
+}
